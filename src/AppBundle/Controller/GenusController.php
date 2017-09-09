7304cec2dbfd6e1a6f675e441e4852a8920062a9
Mikhail <mikhail.khvorostianenko@gmail.com>
2017-09-09 19:50:11 +0300

    26(3.3) Inserting new Objects

--- src/AppBundle/Controller/GenusController.php
+++ src/AppBundle/Controller/GenusController.php
@@ -3,6 +3,7 @@
 namespace AppBundle\Controller;
 
 
+use AppBundle\Entity\Genus;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
@@ -11,6 +12,23 @@ use Symfony\Component\HttpFoundation\Response;
 
 class GenusController extends Controller
 {
+    /**
+     * @Route("/genus/new")
+     */
+    public function newAction()
+    {
+        $genus = new Genus();
+        $genus->setName('Octopus'.rand(1, 100));
+        
+        $em = $this->getDoctrine()->getManager();
+        
+        $em->persist($genus);
+        
+        $em->flush();
+        
+        return new Response('<html><body>Genus created!</body></html>');
+    }
+    
     /**
      * @Route("/genus/{genusName}")
      */
