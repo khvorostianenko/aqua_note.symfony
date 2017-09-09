eda1a33bc5abc7031c074ad259ebf85efd96dba9
Mikhail <mikhail.khvorostianenko@gmail.com>
2017-09-09 20:56:39 +0300

    27(3.4) Adding More Columns

--- src/AppBundle/Entity/Genus.php
+++ src/AppBundle/Entity/Genus.php
@@ -23,6 +23,69 @@ class Genus
     private $name;
     
     /**
+     * @ORM\Column(type="string")
+     */
+    private $subFamily;
+    
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $speciesCount;
+    
+    /**
+     * @ORM\Column(type="string")
+     */
+    private $funFact;
+    
+    /**
+     * @return mixed
+     */
+    public function getSubFamily()
+    {
+        return $this->subFamily;
+    }
+    
+    /**
+     * @return mixed
+     */
+    public function getSpeciesCount()
+    {
+        return $this->speciesCount;
+    }
+    
+    /**
+     * @return mixed
+     */
+    public function getFunFact()
+    {
+        return $this->funFact;
+    }
+    
+    /**
+     * @param mixed $subFamily
+     */
+    public function setSubFamily($subFamily)
+    {
+        $this->subFamily = $subFamily;
+    }
+    
+    /**
+     * @param mixed $speciesCount
+     */
+    public function setSpeciesCount($speciesCount)
+    {
+        $this->speciesCount = $speciesCount;
+    }
+    
+    /**
+     * @param mixed $funFact
+     */
+    public function setFunFact($funFact)
+    {
+        $this->funFact = $funFact;
+    }
+    
+    /**
      * @return mixed
      */
     public function getName()
