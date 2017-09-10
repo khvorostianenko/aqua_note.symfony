3dd154e0fdc5039b7e8e3aad7b1a5ada667a4f91
Mikhail <mikhail.khvorostianenko@gmail.com>
2017-09-10 21:35:00 +0300

    42(4.7) Order By with a OneToMany

--- src/AppBundle/Entity/Genus.php
+++ src/AppBundle/Entity/Genus.php
@@ -45,6 +45,7 @@ class Genus
     
     /**
      * @ORM\OneToMany(targetEntity="AppBundle\Entity\GenusNote", mappedBy="genus")
+     * @ORM\OrderBy({"createdAt"="DESC"})
      */
     private $notes;
     
@@ -54,7 +55,7 @@ class Genus
     }
     
     /**
-     * @return mixed
+     * @return ArrayCollection|GenusNote[]
      */
     public function getNotes()
     {
