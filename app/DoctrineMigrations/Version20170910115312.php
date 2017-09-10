0a901e7d1426c8a59580e21eeb5b802d32b934b1
Mikhail <mikhail.khvorostianenko@gmail.com>
2017-09-10 17:07:55 +0300

    39(4.4) JoinColumn & Relations in Fixtures

--- app/DoctrineMigrations/Version20170910115312.php
+++ app/DoctrineMigrations/Version20170910115312.php
@@ -8,7 +8,7 @@ use Doctrine\DBAL\Schema\Schema;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-class Version20170910114113 extends AbstractMigration
+class Version20170910115312 extends AbstractMigration
 {
     /**
      * @param Schema $schema
@@ -18,7 +18,9 @@ class Version20170910114113 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE genus_note ADD genus_id INT DEFAULT NULL');
+        $this->addSql('ALTER TABLE genus_note CHANGE genus_id genus_id INT NOT NULL');
+        $this->addSql('ALTER TABLE genus_note ADD CONSTRAINT FK_6478FCEC85C4074C FOREIGN KEY (genus_id) REFERENCES genus (id)');
+        $this->addSql('CREATE INDEX IDX_6478FCEC85C4074C ON genus_note (genus_id)');
     }
 
     /**
@@ -28,7 +30,9 @@ class Version20170910114113 extends AbstractMigration
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
-        
-        $this->addSql('ALTER TABLE genus_note DROP genus_id');
+    
+        $this->addSql('ALTER TABLE genus_note DROP FOREIGN KEY FK_6478FCEC85C4074C');
+        $this->addSql('DROP INDEX IDX_6478FCEC85C4074C ON genus_note');
+        $this->addSql('ALTER TABLE genus_note CHANGE genus_id genus_id INT DEFAULT NULL');
     }
 }
--- src/AppBundle/Entity/GenusNote.php
+++ src/AppBundle/Entity/GenusNote.php
@@ -40,6 +40,7 @@ class GenusNote
     
     /**
      * @ORM\ManyToOne(targetEntity="AppBundle\Entity\Genus")
+     * @ORM\JoinColumn(nullable=false)
      */
     private $genus;
     
