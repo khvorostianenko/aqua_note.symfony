783992c6c0d69fcc687879ea9e92106a80381de1
Mikhail <mikhail.khvorostianenko@gmail.com>
2017-09-15 08:38:23 +0300

    51(5.6) Injecting the Cache Service

--- src/AppBundle/Service/MarkdownTransformer.php
+++ src/AppBundle/Service/MarkdownTransformer.php
@@ -3,21 +3,37 @@
 namespace AppBundle\Service;
 
 
+use Doctrine\Common\Cache\Cache;
 use Knp\Bundle\MarkdownBundle\Parser\ParserManager;
 
 class MarkdownTransformer
 {
     private $markdownParser;
+    private $cashe;
     
-    public function __construct(ParserManager $markdownParser)
+    public function __construct(ParserManager $markdownParser, Cache $cashe)
     {
-    
         $this->markdownParser = $markdownParser;
+        $this->cashe = $cashe;
     }
     
     public function parse($str)
     {
-        return $this->markdownParser
+        $cache = $this->cashe;
+    
+        $key = md5($str);
+        
+        if ($cache->contains($key)) {
+            return $cache->fetch($key);
+        }
+        
+        sleep(2);
+        
+        $str = $this->markdownParser
             ->transform($str);
+    
+        $cache->save($key, $str);
+        
+        return $str;
     }
 }
